fa9b4ced47fc859d5b2f5a547f55a929f21f8546
wesley <wesleysantos20012001@gmail.com>
2022-06-11 19:00:17 -0300

    ***VERSÃO FINAL***formatação url com texto variavel; bonus-2; responsividade ***VERSÃO FINAL***

--- script.js
+++ script.js
@@ -94,9 +94,6 @@ function TOTAL(){
     //imprime o valor total na tela de fechamento de pedido
     let item4 = document.querySelector(".totalPrice");
     item4.innerHTML = `R$ ${totalPedido}.00`
-
-
-
 }   
 
 
@@ -104,13 +101,47 @@ function TOTAL(){
 function cancelar(elemento){
     let cancelar = document.querySelector(".fundoBranco");
     cancelar.classList.add("fecharPedido");
+
+
+  
 }
 
-function cancelar(elemento){
-    let cancelar = document.querySelector(".fundoBranco");
-    cancelar.classList.add("fecharPedido");
+function whatsApp(){
+    let NOME = prompt("Qual seu nome?");
+    let ENDERECO = prompt("Qual seu endereço?");
+    let preco1 = document.querySelector(".preco1");
+    preco1.innerHTML = document.querySelector(".selecionado .optionPrice").innerHTML;
+    let preco1Number = Number(preco1.innerHTML.replace('R$',''));
+     
+    let preco2 = document.querySelector(".preco2");
+    preco2.innerHTML = document.querySelector(".selecionado1 .optionPrice").innerHTML;
+    let preco2Number = Number(preco2.innerHTML.replace('R$',''));
+  
+    let preco3 = document.querySelector(".preco3");
+    preco3.innerHTML = document.querySelector(".selecionado2 .optionPrice").innerHTML;
+    let preco3Number = Number(preco3.innerHTML.replace('R$',''));
+      
+  
+    let totalPedido = preco1Number + preco2Number + preco3Number;
+  
+    let item1 = document.querySelector(".item1");
+    let item2 = document.querySelector(".item2");
+    let item3 = document.querySelector(".item3");
+  
+      
+    let meuPedido = (`Olá, gostaria de fazer o pedido:\n
+    - Prato: ${item1.innerHTML}\n
+    - Bebida: ${item2.innerHTML}\n
+    - Sobremesa: ${item3.innerHTML}\n
+    Total: R$${totalPedido}.00\n
+    \n
+    Nome: ${NOME}
+    Endereço: ${ENDERECO}`);
+
+    let meuLink = encodeURIComponent(meuPedido);
+      
+    //`https://wa.me/+5511990174306?text=${meuLink}`
+
+    let divLink = document.querySelector(".link");
+    divLink.innerHTML = "<a href='https://wa.me/+5511990174306?text=" + meuLink + "'target='_blank'><p>Tudo certo, pode pedir!</p> </a>";
 }
-
-
-
-
